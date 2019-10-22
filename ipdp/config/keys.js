22aed1db6ffc5b3987fb70f139c382a464e11435
RMertz <antonmertz@gmail.com>
2019-10-22 17:54:34 -0600

    Update to production collection in /config/keys.js

--- ipdp/config/keys.js
+++ ipdp/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-				MongoURI: 'mongodb+srv://antonmertz:Dash2013!@cluster0-phpry.mongodb.net/test?retryWrites=true&w=majority'
+				MongoURI: 'mongodb+srv://antonmertz:Dash2013!@cluster0-phpry.mongodb.net/ipdp_production?retryWrites=true&w=majority'
 }
