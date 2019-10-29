8124a3d3a13169e6dea1432d5cace2e3f3191baa
RMertz <antonmertz@gmail.com>
2019-10-29 11:26:45 -0600

    Questionaire is updating database properly

--- ipdp/app.js
+++ ipdp/app.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+app.use('/dashboard', require('./routes/dashboard'));
 
 const PORT = process.env.PORT || 5000;
 
